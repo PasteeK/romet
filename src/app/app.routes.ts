cdccd92f219b3387ad149bc74cd57cab1f0ff383
Alexis <desousafreitasalexis@gmail.com>
2025-07-09 16:18:42 +0200

    Added Title Screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TitleScreenComponent } from './ui/title-screen/title-screen';
+import { TitleMenu } from './ui/title-menu/title-menu';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TitleScreenComponent },
+    { path: 'titlemenu', component: TitleMenu },
+];
---
+++ src/app/ui/title-menu/title-menu.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-title-menu',
+  imports: [],
+  templateUrl: './title-menu.html',
+  styleUrl: './title-menu.css'
+})
+export class TitleMenu {
+
+}
